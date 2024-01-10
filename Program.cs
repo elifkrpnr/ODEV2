3d6cd8e2d0f08e52b3710ee6faec3a0259045c0c
efeery <efeeryildirim109@gmail.com>
2024-01-10 11:05:48 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿namespace odev_2.Program
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("1. sayı giriniz");
+            Byte baslangic = byte.Parse(Console.ReadLine());
+            Console.WriteLine("2. sayı giriniz");
+            Byte maksimum = byte.Parse(Console.ReadLine());
+            Console.WriteLine("Sayıların m");
+            Byte adet = byte.Parse(Console.ReadLine());
+            int[] sayilar = new int[baslangic];
+            Random rnd = new Random();
+            int tutulan;
+            for (int i = 1; i < adet; i++)
+            {
+                tutulan = rnd.Next(baslangic, maksimum);
+                for (int j = 0; j < i; j++)
+                {
+                    if (sayilar[j] == tutulan)
+                    {
+                        i--;
+                        break;
+
+                    }
+                    else
+                    {
+                        sayilar[i] = tutulan;
+                    }
+                }
+
+            }
+            for (int i = 1; i < sayilar.Length; i++)
+            {
+                Console.WriteLine(sayilar[i]);
+            }
+        }
+    }
+}
